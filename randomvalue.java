91b437cc9ff1c8c565675036aa11750e3fde36f8
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2022-11-21 14:36:29 -0800

    Add:random value

---
+++ randomvalue.java
@@ -0,0 +1,10 @@
+package snakesladder;
+import java.util.Random;
+public class randomvalue {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int diceValue = rand.nextInt(6)+1;
+        System.out.println("Dice Value is " +diceValue);
+    }
+}

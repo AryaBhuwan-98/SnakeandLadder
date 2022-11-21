763c41b154d60eff4d75b0fae1e8802772d316f8
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2022-11-21 14:29:52 -0800

    Add:Game Start Position

---
+++ startposition.java
@@ -0,0 +1,8 @@
+package snakesladder;
+public class startposition {
+	public static void main(String[] args) {
+	    int pos = 0;
+	    System.out.println("Welcome to Snake And Ladder program");
+	    System.out.println("Single player at the start position "+ pos);
+	}
+	}
